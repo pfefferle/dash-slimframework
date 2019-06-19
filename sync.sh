a6f4be9fe22ae7a642fd3f884a2b38878f1256a2
Filip Š <filip.stamcar@hotmail.com>
2019-06-19 14:41:28 +0200

    Add support for Python 3 & Add new Slim 3 URL

--- sync.sh
+++ sync.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+rm -rf Slim_Framework.docset/Contents/Resources
+
 httrack http://www.slimframework.com/docs/ \
   -O Slim_Framework.docset/Contents/Resources/Documents,cache -I0 \
   --display=2 --timeout=60 --retries=99 --sockets=7 \
   --connection-per-second=5 --max-rate=250000 \
   --keep-alive --depth=5 --mirror --clean --robots=0 \
   --user-agent '$(httrack --version); dash-slimframework ()' \
-  "-*" "+www.slimframework.com/docs" "+www.slimframework.com/docs/*" \
+  "-*" "+www.slimframework.com/docs" \
+  "+www.slimframework.com/docs/v3" "+www.slimframework.com/docs/v3/*" \
   "+www.slimframework.com/assets/*" "+maxcdn.bootstrapcdn.com/font-awesome/*"
